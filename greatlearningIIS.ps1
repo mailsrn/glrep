f22b1638d5d37f01f9a7dd6a5bf5c189d87236ba
mailsrn <48926143+mailsrn@users.noreply.github.com>
2020-02-10 15:15:36 +0530

    Update greatlearningIIS.ps1

--- greatlearningIIS.ps1
+++ greatlearningIIS.ps1
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Add-WindowsFeature Web-Server
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value "Azure Project – Great Learning!!!"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Project – Great Learning!!!"
